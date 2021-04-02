5138c3e7469a6285ba22795e1ede72663b3b13cf
Emeric <emeric.dynomant@gmail.com>
2021-04-02 13:46:59 +0000

    wip

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import numpy as np
 
 page = requests.get("https://en.wikipedia.org/wiki/Prices_of_chemical_elements").text
 chemicals_price = pd.read_html(page, flavor="bs4")[0]
@@ -10,7 +11,36 @@ human_composition = pd.read_html(page, flavor="bs4")[1]
 
 print(list(human_composition["Fraction of mass[11][12][13][14][15][16]"]))
 
-for n, k, l in zip(list(chemicals_price["Name"]["Name"]), list(chemicals_price["Price[5]"]["USD/kg"]), list(chemicals_price["Price[5]"]["USD/L[c]"])):
-    print(f"{n}\t{k} / {l}")
-    print(human_composition.loc[human_composition['Element'] == n, 'Fraction of mass[11][12][13][14][15][16]'])
-    print()
+
+POIDS = 49.9
+print()
+FINAL_TOT = 0
+
+for n, k in zip(list(chemicals_price["Name"]["Name"]), list(chemicals_price["Price[5]"]["USD/L[c]"])):
+    fraction = human_composition.loc[human_composition['Element'] == n, 'Fraction of mass[11][12][13][14][15][16]']
+
+    # If this element is found in the body
+    if len(list(fraction)) > 0 and list(fraction)[0] is not np.nan:
+        fraction = fraction.values[0]
+        
+        # Numeric fraction
+        try:
+            fraction = float(fraction)
+        except:
+            unit = float(fraction.split("×")[0])
+            exposant = fraction.split("×")[1].replace("10", "").replace("−", "-")
+            fraction = float(f"{unit}E{exposant}")
+
+        # Numeric price
+        try:
+            price = float(k)
+        except:
+            k = [float(x) for x in k.split("–")]
+            price = np.mean(k)
+
+        total = price * fraction * POIDS
+        FINAL_TOT = FINAL_TOT + total
+
+        print(f"{n}\t{price}$/k * fraction: {fraction} * {POIDS} = {total}$")
+
+print(f"FINAL: {round(FINAL_TOT, 2)}$")
